b83414db0ef27bf44956257dfc1fab4f2fcae35d
wjyoem <wjyoem@163.com>
2017-10-22 18:04:12 +0800

    2
    2

--- js/live-room.js
+++ js/live-room.js
@@ -1,5 +1,4 @@
 !function(e) {
-	alert(1);
     function t(r) {
         if (n[r])
             return n[r].exports;
@@ -4639,31 +4638,7 @@
 }
 , function(e, t) {
     e.exports = {
-        render: function() {
-            var e = this
-              , t = e.$createElement;
-            e._self._c;
-            return e._m(0)
-        },
-        staticRenderFns: [function() {
-            var e = this
-              , t = e.$createElement
-              , n = e._self._c || t;
-            return n("div", {
-                attrs: {
-                    id: "J_header-bar"
-                }
-            }, [n("div", {
-                staticClass: "logo-wrapper"
-            }, [n("img", {
-                staticClass: "header-icon f-inline",
-                attrs: {
-                    src: "//gw.alicdn.com/mt/TB1QXiPNpXXXXb4aXXXXXXXXXXX-256-54.png",
-                    alt: ""
-                }
-            })])])
-        }
-        ]
+        
     }
 }
 , function(e, t, n) {
